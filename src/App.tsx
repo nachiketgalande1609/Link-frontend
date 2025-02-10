78b6c4ce1d121c6a46603a7b246b0ec68aaa0289
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-02-11 01:30:40 +0530

    Warnings fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Box, ThemeProvider } from "@mui/material";
 
 import { useUser } from "./context/userContext";
@@ -122,6 +122,7 @@ const AppContent = () => {
         };
 
         socket.on("notificationAlert", handleNotificationAlertResponse);
+        console.log(notificationAlert);
 
         return () => {
             socket.off("notificationAlert", handleNotificationAlertResponse);
